3567c7d632317888363afe10a8a6d999b6b8f469
Neha Kondabathini <nehakondabathini1234@gmail.com>
2025-12-17 17:35:12 +1030

    Partially Implemented Keyboard

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
-
-
 let attempt=1;
 let currentString="";
 let word="APPLE";
+
+
+document.getElementById('box1').focus();
 
 readyBox();
 
@@ -12,21 +13,39 @@ readyBox();
 function readyBox(){
     for (let i = 5*(attempt-1)+1; i <= 5*attempt; i++) {
         document.getElementById(`box${i}`).addEventListener("keydown", function(event){
+
             if(event.key==="Enter"){
                 getUserInput();
+            }
+
+
+        })
+
+        document.getElementById(`box${i}`).addEventListener("keyup",function(event){
+            if(i!==5*attempt){
+                let Box=document.getElementById(`box${i}`);
+                if(Box.value.length>=Box.maxLength){
+                    document.getElementById(`box${i+1}`).focus();
+                }
+            }
+
+            if(event.key==="Backspace") {
+                if (i !== 5 * (attempt - 1) + 1 ) {
+                    document.getElementById(`box${i - 1}`).focus();
+                }
             }
         })
     }
 
 }
 
-function match(letter, index){
-    if(word[index]===letter){
+function match(letter, index) {
+    if (word[index] === letter) {
         return "correct";
     }
 
-    for(let i=0;i<word.length;i++){
-        if(word[i]===letter){
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
             return "present";
         }
     }
@@ -36,6 +55,8 @@ function match(letter, index){
 
 
 function getUserInput(){
+
+    currentString = "";
     for (let i = 5*(attempt-1)+1; i <= 5*attempt; i++) {
         currentString+=document.getElementById(`box${i}`).value;
     }
@@ -44,6 +65,7 @@ function getUserInput(){
         return;
     }
 
+
     currentString=currentString.toUpperCase();
 
     console.log(currentString);
@@ -51,16 +73,23 @@ function getUserInput(){
     for(let i=0;i<currentString.length;i++){
         let result=match(currentString[i],i);
 
+        let box=document.getElementById(`box${5*(attempt-1)+1+i}`);
         if(result==="correct"){
-            let box=document.getElementById(`box${5*(attempt-1)+1+i}`);
             box.style.backgroundColor="#538d4e";
-        }else if(result==="present"){
-            let box=document.getElementById(`box${5*(attempt-1)+1+i}`);
+            let key=document.getElementById(currentString[i]);
+            key.style.backgroundColor="#538d4e";
+        }else if(result==="present" ){
             box.style.backgroundColor="#b59f3b";
-        }else{
-            let box=document.getElementById(`box${5*(attempt-1)+1+i}`);
-            box.style.backgroundColor="#3a3a3c";
+            let key=document.getElementById(currentString[i]);
+            key.style.backgroundColor="#b59f3b";
         }
+        else{
+            box.style.backgroundColor="#3a3a3c";
+            let key=document.getElementById(currentString[i]);
+            key.style.backgroundColor="#3a3a3c";
+        }
+
+        box.readOnly=true;
     }
 
     if(currentString===word){
@@ -68,6 +97,7 @@ function getUserInput(){
     }
 
     currentString="";
+    document.getElementById(`box${5*attempt+1}`).focus();
     attempt+=1;
 
     for (let i = 5*(attempt-1)+1; i <= 5*attempt; i++) {
@@ -77,4 +107,13 @@ function getUserInput(){
     readyBox();
 
 }
+
+
+
+
+
+
+
+
+
 
