7d22002b260c2029217bd23450f1527909ad601a
Neha Kondabathini <nehakondabathini1234@gmail.com>
2026-02-27 11:22:49 +1030

    Connected to MongoDb Atlas

--- scripit/server.js
+++ scripit/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -13,11 +13,17 @@ const authRoutes = require('./router/auth');
 app.use('/api/auth', authRoutes);
 
 
+const dbURI=process.env.MONGODB_URI;
 
+mongoose.connect(dbURI).then(()=>{
+    console.log("Connected to MongoDB Atlas")})
+    .catch((err)=>{
+        console.error(err);
+})
 
-mongoose.connect('mongodb://127.0.0.1:27017/wordleDatabase')
+/*mongoose.connect('mongodb://127.0.0.1:27017/wordleDatabase')
     .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error(err));
+    .catch(err => console.error(err)); */
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
